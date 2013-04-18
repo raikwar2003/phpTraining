67e4509abcecf9a8a61bc1431da1d90e3903fc89
Ravi Raikwar <raikwar2003@yahoo.co.in>
2013-04-18 17:36:12 +0530

    array examples with for and foreach loop

--- example.php
+++ example.php
@@ -1,28 +1,44 @@
 <?php
-echo "Hello World";
+	$car0="Volvo";
+	$car1="BMW";
+	$car2="Toyota";
+	$cars=array($car0,$car1,$car2);
+	echo "I like ".$cars[0].", ".$cars[1].", ".$cars[2].".<br />";
+?>
+<?php  echo"-----------------------------------<br />"; ?>
+<?php
+	$car0="Volvo";
+	$car1="BMW";
+	$car2="Toyota";
+	$cars=array($car0,$car1,$car2);
+	echo count ($cars);
+	echo " cars</br>";
+?>
+<?php  echo"-----------------------------------<br />"; ?>
+<?php
+	$car0="Volvo";
+	$car1="BMW";
+	$car2="Toyota";
+	$cars=array($car0,$car1,$car2);
+	$arrlength=count($cars);
+	
+	for ($i=0; $i<$arrlength; $i++){
+		echo "I love"."\n".$cars[$i].".<br>";
+	}
+?>
+<?php  echo"-----------------------------------<br />"; ?>
+<?php
+	$age=array("Peter"=>"35","Ben"=>"37","Joe"=>"40");
+	echo "Peter is ".$age["Peter"]." years old </br>";
+	echo "Ben is ".$age["Ben"]." years old </br>";
+	echo "Joe is ".$age["Joe"]." years old </br>";
+?>
+<?php  echo "-----------------------------------<br />"; ?>
+<?php
+	$age=array("Peter"=>"35","Ben"=>"37","Joe"=>"40");
+	
+	foreach($age as $x=>$x_value ){
+		echo $x.$x_value."<br />";
+	}
 ?>
 
-<?php 
-	$a = "Hello World";
-	$b = "2013";
-	$c = "13"
-?>
-
-<h4 class="span12">
-	<?php 
-		//echo "$a"."$b" ;
-		echo "$a ";
-		echo " $b-$c";
-		echo "=".$b-$c;
-	?>
-</h4>
-
-<h4>
-	<?php
-		$out = 10;
-		
-		function printVal() {
-			print $out;	
-		}
-	?>
-</h4>
