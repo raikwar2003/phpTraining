05472ad81372d5706dfcdcd2409f41df90da1aec
Ravi Raikwar <raikwar2003@yahoo.co.in>
2013-04-19 17:40:52 +0530

    PHP array_functions

--- example.php
+++ example.php
@@ -41,4 +41,93 @@
 		echo $x.$x_value."<br />";
 	}
 ?>
+<?php  echo "-----------------------------------<br />"; ?>
+<?php
+	$cars=array("Volvo","BMW","Toyota");
+	sort($cars);
+	
+	$arrlength=count($cars);
+	for($i=0; $i<$arrlength; $i++){
+		echo $cars[$i]."<br/>";
+	}
+?>
+<?php  echo "-----------------------------------<br />"; ?>
+<?php
+	$cars=array("Volvo","BMW","Toyota");
+	rsort($cars);
+	
+	$arrlength=count($cars);
+	for($i=0; $i<$arrlength; $i++){
+		echo $cars[$i]."<br/>";
+	}
+?>
+<?php  echo "-----------------------------------<br />"; ?>
+<?php
+	$car0="Volvo";
+	$car1="Nissan";
+	$car2="Toyota";
+	$car3="Jaguar";
+	$cars=array($car0=>"100", $car3=>"89", $car2=>"55", $car1=>"100");
+	krsort($cars);
+	
+	foreach($cars as $car=>$car_value){
+		echo "Key=".$car."\n Value=".$car_value."<br/>";
+	}
+?>
+<?php  echo "-----------------------------------<br />"; ?>
+<?php
+	$car0="Volvo";
+	$car1="Nissan";
+	$car2="Toyota";
+	$car3="Jaguar";
+	$cars=array($car0=>"100", $car3=>"89", $car2=>"55", $car1=>"100");
+	
+	print_r(array_change_key_case($cars,CASE_LOWER));
+	echo "<br/>";
+?>
+<?php  echo "-----------------------------------<br />"; ?>
+<?php
+	$car0="Volvo";
+	$car1="Nissan";
+	$car2="Toyota";
+	$car3="Jaguar";
+	$cars=array($car0=>"100", $car3=>"89", $car2=>"55", $car1=>"100");
+	
+	print_r(array_change_key_case($cars,CASE_UPPER));
+	echo "<br/>";
+?>
+<?php  echo "-----------------------------------<br />"; ?>
+<?php
+	$car0="Volvo";
+	$car1="Nissan";
+	$car2="Toyota";
+	$car3="Jaguar";
+	$cars=array($car0=>"100", $car1=>"89", $car2=>"55", $car3=>"100");
+	echo "array_chunk <br/>",
+	print_r(array_chunk($cars,1));
+	echo "<br/>";
+?>
+<?php  
+	echo "-----------------------------------<br />"; 
+	$name=array("Rahul", "Rohit", "Ronit");
+	$age=array("25", "24", "26");
+	$c=array_combine($name, $age);
+	print_r($c);
+?>
+
+<?php  
+	echo "<br />-----------------------------------<br />"; 
+	$name=array("Rahul", "23", "Ronit","Rahul", "23", "23");
+	//$c=array_combine($name, $age);
+	print_r(array_count_values($name));
+?>
+
+<?php  
+	echo "<br />-----------------------------------<br />"; 
+	$name1=array("Rohit","Ronit","Rahul","Ravi");
+	$name2=array("Rohit","Ronit","Rahul","Sonam");
+	
+	$result=array_diff($name1,$name2);
+	print_r($result);
+?>
 
